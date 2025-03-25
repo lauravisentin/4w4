1bbbc3385f74721e9036b79463774d88171919b1
lauravisentin <laura.vise@icloud.com>
2025-03-25 11:26:37 -0400

    Labo4:Page 404; dimensions et menu

--- 404.php
+++ 404.php
@@ -1,12 +1,13 @@
 <?php get_header() ?>
 
 <div class="boiteflex global erreur">
+    
     <h1 class="erreur__titre">Erreur 404</h1>
     <p class="erreur__message">Désolé, la page que vous recherchez n'existe pas.</p>
 
-    <nav class="navigation">
-        <ul class="navigation__liste">
-            <li class="navigation__element">
+    <nav class="erreur__navigation">
+        <ul>
+            <li>
                 <a href="accueil" class="navigation__lien">Accueil</a>
             </li>
         </ul>
